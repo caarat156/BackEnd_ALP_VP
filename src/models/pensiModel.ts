e1f66659bf6632f5b44b35216e7b9cf94af369fe
Angelique1701 <angeliquekw50@gmail.com>
2025-12-13 17:22:49 +0700

    rapiin dikit

--- src/models/pensiModel.ts
+++ src/models/pensiModel.ts
@@ -1,17 +1,17 @@
 import { z } from "zod";
 
 export const PensiModel = {
-Create: z.object({
-name: z.string(),
-date: z.string(),
-location: z.string(),
-price: z.number(),
-}),
-Payment: z.object({
-pensiId: z.number(),
-userId: z.number(),
-amount: z.number(),
-}),
+    Create: z.object({
+    name: z.string(),
+    date: z.string(),
+    location: z.string(),
+    price: z.number(),
+    }),
+    Payment: z.object({
+    pensiId: z.number(),
+    userId: z.number(),
+    amount: z.number(),
+    }),
 };
 
 export type CreatePensiDTO = z.infer<typeof PensiModel.Create>;
